b6b08b73c10d46e5175329a94aa2d20420714dd9
ricardo <ricardo.batista07@gmail.com>
2015-01-19 21:00:44 -0400

    Initial Commit

---
+++ Imaginarium/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Imaginarium
+//
+//  Created by Ricardo Batista on 1/19/15.
+//  Copyright (c) 2015 XdroneStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
