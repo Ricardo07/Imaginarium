f62d087b60eaa72368230c30ea2b93dfbc2f833b
ricardo <ricardo.batista07@gmail.com>
2015-01-20 11:26:22 -0400

    Lecture 10 Imaginarium demo finished.

--- Imaginarium/ImageViewController.h
+++ Imaginarium/ImageViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ImageViewController.h
 //  Imaginarium
 //
 //  Created by Ricardo Batista on 1/19/15.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ImageViewController : UIViewController
+@property (strong, nonatomic) NSURL *imageURL;
 @end
--- Imaginarium/ViewController.m
+++ Imaginarium/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ImageViewController.h"
 
 @interface ViewController ()
 
@@ -14,14 +15,24 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    
+    ImageViewController *ivc = (ImageViewController *)segue.destinationViewController;
+    
+    if([segue.identifier isEqualToString:@"photo_1"]){
+        ivc.imageURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.bimmertoday.de/wp-content/uploads/2014-BMW-M4-Rot-F82-Concept-01.jpg"]];
+        ivc.title = @"Front";
+    }
+    
+    if([segue.identifier isEqualToString:@"photo_2"]){
+        ivc.imageURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.motorward.com/wp-content/images/2014/07/Sakhir-Orange-BMW-M4-1.jpg"]];
+        ivc.title = @"Back";
+    }
+    
+    if([segue.identifier isEqualToString:@"photo_3"]){
+        ivc.imageURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://assets.nydailynews.com/polopoly_fs/1.1886091.1406754666!/img/httpImage/image.jpg_gen/derivatives/article_970/2015-bmw-m4.jpg"]];
+        ivc.title = @"Running";
+    }
 }
 
 @end
